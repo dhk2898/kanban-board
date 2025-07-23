94f3d20d698d0b9456a8488a1e960558fce2af0a
Dom Kim <dhk2898@hotmail.com>
2025-07-24 01:30:58 +0900

    added drag and drop functionality. TODO: CSS styling

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,6 +2,7 @@ import { useKanban } from "./KanbanContext";
 import List from "./List.tsx";
 import {v4 as uuidv4} from 'uuid';
 import { useState } from "react";
+import { DragDropContext, Draggable, Droppable, type DropResult } from "@hello-pangea/dnd";
 
 
 function Board(){
@@ -19,14 +20,54 @@ function Board(){
   setNewListTitle('');
  }
 
+ function handleDragEnd(result: DropResult){
+    const {destination, source, draggableId, type} = result;
+    if (!destination) return;
+
+    if (type === 'list'){
+        dispatch({
+            type: "move-list",
+            sourceIndex: source.index,
+            destIndex: destination.index,
+        });
+
+        return;
+    }
+
+    if (source.droppableId === destination.droppableId && source.index === destination.index)
+    {
+        return;
+    }
+
+    dispatch({
+        type: 'move-task',
+        taskId: draggableId,
+        sourceListId: source.droppableId,
+        destListId: destination.droppableId,
+        destIndex: destination.index,
+    });
+ }
+
  return(
-  <div>
-   {state.listOrder.map(listId => (<List key = {listId} listId ={listId}/>))}
-   <div>
-    <input type = 'text' placeholder = 'New List Title' value = {newListTitle} onChange = {(e) => setNewListTitle(e.target.value)}/>
-    <button onClick = {handleAddList}>Add List</button>
-   </div>
-  </div>
+    <DragDropContext onDragEnd={handleDragEnd}>
+    <div>
+        <Droppable droppableId="all-lists" direction="horizontal" type="list">
+            {(provided) => (<div ref={provided.innerRef} {...provided.droppableProps}>
+                {state.listOrder.map((listId, index) => 
+                (<Draggable key ={listId} draggableId={listId} index = {index}>
+                    {(provided) => (<div ref = {provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                        <List listId={listId} />
+                    </div>)}
+                </Draggable>))}
+                {provided.placeholder}
+                </div>)}
+        </Droppable>
+        <div>
+            <input type = 'text' placeholder = 'New List Title' value = {newListTitle} onChange = {(e) => setNewListTitle(e.target.value)}/>
+            <button onClick = {handleAddList}>Add List</button>
+        </div>
+    </div>
+    </DragDropContext>
  );
 
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,3 +1,4 @@
+import { Draggable, Droppable } from "@hello-pangea/dnd";
 import { useKanban } from "./KanbanContext";
 import Task from "./Task.tsx";
 import { useState } from "react";
@@ -18,13 +19,27 @@ function List ({listId}: {listId: string}){
  return(
  <div className = 'list-container'>
   <h3>{list.title}</h3>
-  {list.taskIds.map(taskId => (
-   <Task key = {taskId} taskId = {taskId} />
-  ))}
-  <input
-  value = {newTaskContent}
-  onChange = {e => setNewTaskContent(e.target.value)}
-  placeholder="New Task"/>
+  <Droppable droppableId={listId} type="task">
+    {(provided) => (
+        <div ref={provided.innerRef} {...provided.droppableProps}>
+        {list.taskIds.map((taskId, index) => (
+            <Draggable draggableId={taskId} index={index} key={taskId}>
+            {(provided) => (
+                <div
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                >
+                <Task taskId={taskId} />
+                </div>
+            )}
+            </Draggable>
+        ))}
+        {provided.placeholder}
+        </div>
+    )}
+  </Droppable>
+  <input value = {newTaskContent} onChange = {e => setNewTaskContent(e.target.value)} placeholder="New Task"/>
   <button onClick = {handleAddTask}>Add Task</button>
  </div>)
 }
